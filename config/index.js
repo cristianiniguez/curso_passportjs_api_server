7228f0672a1ac2be907d95534ea705a562bb0adf
cristianiniguez <felizpc1@gmail.com>
2020-11-06 18:48:07 -0400

    configuracion de passportjs

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
@@ -7,7 +7,12 @@ const config = {
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME
-}
+  dbName: process.env.DB_NAME,
+  defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+  defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
+};
 
-module.exports = { config }
+module.exports = { config };
