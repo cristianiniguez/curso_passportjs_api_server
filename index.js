448b3e435ec23f7d8e061135750ae848f3f0db44
cristianiniguez <felizpc1@gmail.com>
2020-11-07 14:57:05 -0400

    implementacion de nuestro sign-in

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const { config } = require('./config/index');
 
+const authApi = require('./routes/auth');
 const moviesApi = require('./routes/movies');
 const userMoviesApi = require('./routes/userMovies');
 
@@ -19,6 +20,7 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 app.use(express.json());
 
 // routes
+authApi(app);
 moviesApi(app);
 userMoviesApi(app);
 
