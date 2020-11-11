e23137dfec13757a2a25fa7686e56c9e0d306503
cristianiniguez <felizpc1@gmail.com>
2020-11-11 12:57:01 -0400

    seguridad con helmet

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -18,6 +19,7 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
+app.use(helmet());
 
 // routes
 authApi(app);
